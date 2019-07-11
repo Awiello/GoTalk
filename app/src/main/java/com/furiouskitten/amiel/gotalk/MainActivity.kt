ebd78c1407cd42dea8271a13754bd01f66dc4f76
Amiel Cueto <amielcueto1997@gmail.com>
2019-07-11 20:25:26 +0800

    Done with MainActivity UI setup :D

--- app/src/main/java/com/furiouskitten/amiel/gotalk/MainActivity.kt
+++ app/src/main/java/com/furiouskitten/amiel/gotalk/MainActivity.kt
@@ -11,6 +11,7 @@ import android.support.design.widget.NavigationView
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
 import android.view.Menu
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
@@ -23,13 +24,28 @@ class MainActivity : AppCompatActivity() {
 
         val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
         val navView: NavigationView = findViewById(R.id.nav_view)
-        val toggle = ActionBarDrawerToggle(
-            this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close
-        )
+        val toggle = ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
         drawerLayout.addDrawerListener(toggle)
         toggle.syncState()
 
     }
+
+
+
+    fun loginButtonOnClicked(view: View){
+
+    }
+
+   fun addChannelClicked(view: View){
+
+    }
+
+    fun sendMessageOnClicked(view: View){
+
+    }
+
+
+
 
     override fun onBackPressed() {
         val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
